aa400258639725c8c223b288bbaca6ed8fced3f7
laoderidho <2081007@unai.edu>
2023-03-22 15:37:35 +0700

    finish NPM

---
+++ fundamental_front_end/NPM/src/script/index.js
@@ -0,0 +1,14 @@
+//jquery code time display
+
+const displayTime = ()=>{
+    moment.locale('id');
+    $('.time').text(moment().format('LTS'));
+    $('.date').text(moment().format('LL'));
+};
+
+const updateTime = ()=>{
+    displayTime()
+    setTimeout(updateTime, 1000)
+}
+
+updateTime();
